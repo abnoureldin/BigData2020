6d14ef91c945dcb591f525f04d40c1e89da7e30a
abnoureldin <abdel.noureldin@appexperts.me.uk>
2020-09-03 16:04:22 +0100

    stuck

--- task6/producer.py
+++ task6/producer.py
@@ -5,6 +5,5 @@ producer = KafkaProducer(bootstrap_servers='localhost:9099')
 with open('/home/hadoop/BigData2020/Shakespeare.txt') as f:
 	for line in f:
 		producer.send('shakespeare',line.encode('ascii'))
-
-producer.flush()
+		producer.flush()
 producer.close()
