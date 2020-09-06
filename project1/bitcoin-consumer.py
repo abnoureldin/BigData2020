ce2b1c8c1f5f4840eac529347ad0c25a8c4c1b16
abnoureldin <abdel.noureldin@appexperts.me.uk>
2020-09-06 13:14:56 +0100

    consumer update

--- project1/bitcoin-consumer.py
+++ project1/bitcoin-consumer.py
@@ -1,6 +1,18 @@
-from kafka import KafkaConsumer
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
 
-consumer = KafkaConsumer('kraken',bootstrap_servers = 'localhost:9099')
+def main():
+	sc = SparkContext(appName='Bitcoin')
+	ssc = StreamingContext(sc,2)
+	broker,topic = "localhost:9099","kraken"
+	kvs = KafkaUtils.createDirectStream(ssc,[topic],
+					{"metadata.broker.list":broker})
+	lines = kvs.map(lambda x: x[1])
+	lines.foreachRDD(lambda rdd: rdd.toDF())
+	lines.pprint()
+	ssc.start()
+	ssc.awaitTermination()
 
-for msg in consumer:
-	print(msg.value.decode())
+if __name__ == "__main__":
+	main()
