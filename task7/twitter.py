76557577c25549a917ce2979d9200af40b4d6bce
abnoureldin <abdel.noureldin@appexperts.me.uk>
2020-09-21 10:43:19 +0100

    task7

--- task7/twitter.py
+++ task7/twitter.py
@@ -1,9 +1,39 @@
-from pyspark.sql import SQLContext
-from pyspark import SparkContext
+#spark-submit --jars /opt/mysql-connector-java-8.0.21.jar twitter.py 
 from pyspark.sql import SparkSession
+from pyspark.streaming import StreamingContext
+from pyspark import SparkContext
+from pyspark.sql.functions import *
+import re
 
-ss = SparkSession.builder.appName("tweets").enableHiveSupport().getOrCreate()
+spark = SparkSession.builder.appName("twitter").getOrCreate()
+ssc = StreamingContext(spark.sparkContext,5)
+lines = ssc.textFileStream("hdfs://localhost:9000/twitter")
 
-df = ss.read.format("avro").load("hdfs://localhost:9000/user/twitter/FlumeData.1599577737180")
-#df = ss.read.format("avro").load("file:///home/ab/Downloads/twitter.avro")
-df.show()
+def transform(rdd):
+	if not rdd.isEmpty():
+		df = spark.createDataFrame(rdd,schema=["#","count"])
+		SQL(df)
+
+def SQL(df):
+	df.show()
+	df.write.format("jdbc").mode("overwrite")\
+	.option("driver","com.mysql.jdbc.Driver")\
+	.option("url","jdbc:mysql://localhost:3306/bigdata2020")\
+	.option("dbtable","tweets")\
+	.option("user","root")\
+	.option("password","password")\
+	.save()
+
+counts = lines.flatMap(lambda l:l.split(' ')) \
+    .filter(lambda w:w.lower().startswith('#')) \
+    .map(lambda w: w.replace('#','')) \
+    .map(lambda w: w.lower()) \
+    .filter(lambda w: re.sub(r'[^a-z]+','',w)) \
+    .filter(lambda w: re.sub(r'[^\x00-\x7F]+','',w)) \
+    .map(lambda x: (x,1)) \
+    .reduceByKey(lambda x,y:x+y) \
+    .map(lambda r: (r[0],r[1])) \
+    .foreachRDD(transform)
+
+ssc.start()
+ssc.awaitTermination()
