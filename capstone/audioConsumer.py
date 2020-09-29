ccc0bc118888b4ef6e7e8a1f38f397d7bc423379
abnoureldin <abdel.noureldin@appexperts.me.uk>
2020-09-29 16:28:06 +0100

    mongo

--- capstone/audioConsumer.py
+++ capstone/audioConsumer.py
@@ -1,10 +1,16 @@
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
 from pyspark.sql import Row
 from pyspark.sql.functions import from_json, col, explode
+"""
+spark-submit --conf "spark.mongodb.input.uri=mongodb://127.0.0.1/audio-brain.result?readPreference=primaryPreferred" \
+             --conf "spark.mongodb.output.uri=mongodb://127.0.0.1/audio-brain.result" \
+             --packages org.mongodb.spark:mongo-spark-connector_2.11:2.4.2 \
+			 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.1 audioConsumer.py
+"""
 
 def readRDD(rdd):
 	if not rdd.isEmpty():
@@ -15,16 +21,22 @@ def readRDD(rdd):
 		data = sqlContext.sql("SELECT "+
 							",".join(cols)+
 							" FROM audio_brain")
-		
+
 		result = data.select("result.*")
-		result.show()
+		result.write.format("mongo")\
+		.mode("append").save()
 
 if __name__ == "__main__":
+	working_directory = "jars/*"
 	sc = SparkContext(appName='audio-brain')
 	ssc = StreamingContext(sc,60)
 	sqlContext = SQLContext(sc)
 	spark = SparkSession(sc)
 	sc.setLogLevel("WARN")
+	ss = SparkSession.builder.appName("audio-brain")\
+		.config("spark.mongodb.input.uri", "mongodb://127.0.0.1/audio-brain.result")\
+		.config("spark.mongodb.output.uri", "mongodb://127.0.0.1/audio-brain.result")\
+		.getOrCreate()
 	
 	broker,topic = "localhost:9099","audio-brain"
 	
