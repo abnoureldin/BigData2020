0b36ff3c84c5d6916d13d0bd0e369b49c6749592
abnoureldin <abdel.noureldin@appexperts.me.uk>
2020-09-30 16:44:08 +0100

    credentials

--- task9/spotify.py
+++ task9/spotify.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import json
 from kafka import KafkaProducer
 
-cid = "7cc3f5cf909541dd8f7b82e3bf1d3334"
-secret = "9fffedc1d22c4731943b1c08712e5486"
+cid = str(open("cid.txt","r").read())
+secret = str(open("secret.txt","r").read())
 
 client_credentials_manager = SpotifyClientCredentials(client_id=cid,
 				client_secret=secret)
