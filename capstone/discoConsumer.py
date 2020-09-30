d24208e6e3284dbac6eee645633bd2932f0fd2e9
abnoureldin <abdel.noureldin@appexperts.me.uk>
2020-09-30 15:21:55 +0100

    implemented kafka

--- capstone/discoConsumer.py
+++ capstone/discoConsumer.py
@@ -3,4 +3,7 @@ from kafka import KafkaConsumer
 consumer = KafkaConsumer('track_id',bootstrap_servers='localhost:9099')
 
 for msg in consumer:
-	print(msg.value.decode())
+	x = msg.value.decode()
+	f = open('track.txt','w')
+	print(x, file=f)
+	exit()
